f15f375d23544feb329f9c71f9687b6f0c8a8208
Matthew Seeley <matthew@threadlight.com>
2010-06-27 15:28:49 -0400

    Adds mouse position example in Ruby / Shoes
    http://rosettacode.org/wiki/Mouse_position

---
+++ mouse-position/mouse-position.rb
@@ -0,0 +1,6 @@
+Shoes.app(:title => "Mouse Position", :width => 400, :height => 400) do
+  @position = para "Position : ?,?", :size => 12, :margin => 10
+  motion do |x,y|
+    @position.text = "Position : " + x.to_s + "," + y.to_s 
+  end
+end 
